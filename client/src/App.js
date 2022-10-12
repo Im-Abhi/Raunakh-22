59fbf38e548c5c8dd107bb984807c97b4e11f404
Abhishek Gupta <abhishekgupta942001@gmail.com>
2022-10-12 18:28:14 +0530

    testimonial skeleton

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,13 @@
+import React from "react";
+
+import Testimonials from "./Components/Testimonials";
+
+import "./App.css";
+
 function App() {
   return (
     <div className="App">
-      
+      <Testimonials />
     </div>
   );
 }
