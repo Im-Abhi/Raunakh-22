475f24784b432d878ab110c604f43216b913d442
Abhishek Gupta <abhishekgupta942001@gmail.com>
2022-10-12 18:39:03 +0530

    about skeleton

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Testimonials from "./Components/Testimonials";
+import About from "./Components/About";
 
 import "./App.css";
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <Testimonials />
+      <About />
     </div>
   );
 }
