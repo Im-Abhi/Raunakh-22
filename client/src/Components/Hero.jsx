7055dc4c3c55587c972f4016e9a76b2f86bbfc26
Bobby Kumar <kashyapashish0002@gmail.com>
2022-10-17 23:51:35 +0530

    QR code added for donation

--- client/src/Components/Hero.jsx
+++ client/src/Components/Hero.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import { BsFillPlayCircleFill } from 'react-icons/bs'
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
-import razorpayBtn from '../Assets/gallery/RazorpayBtnImage.png';
+import qr from '../Assets/gallery/qr.jpeg';
+// import razorpayBtn from '../Assets/gallery/RazorpayBtnImage.png';
 
 const Hero = () => {
     return (
         <>
-            <section className="py-8 md:py-12 lg:py-20 min-h-[75vh]" id='home'>
+            <section className="py-8 min-h-[75vh]" id='home'>
                 <div id='stars'></div>
                 <div id='stars2'></div>
                 <div id='stars3'></div>
                 <div className="flex flex-col gap-8 sm:flex-row-reverse sm:w-11/12 sm:mx-auto">
-                    <div className="flex flex-col gap-6 sm:flex-1 sm:justify-center md:flex-row md:items-center">
+                    {/* <div className="flex flex-col gap-6 sm:flex-1 sm:justify-center md:flex-row md:items-center">
                         <div className="w-max mx-auto text-center">
                             <h1 className="text-5xl font-medium text-[#EAB308] lg:text-7xl">129</h1>
                             <p className="uppercase text-lg lg:text-xl">People Donated</p>
@@ -21,7 +22,7 @@ const Hero = () => {
                             <h1 className="text-5xl font-medium text-[#22C55E] lg:text-7xl">₹ 23973</h1>
                             <p className="uppercase text-lg lg:text-xl">Donation Recieved</p>
                         </div>
-                    </div>
+                    </div> */}
                     <div className="flex flex-col gap-6 sm:flex-1 sm:justify-center md:gap-8 lg:gap-12">
                         <div className="w-max mx-auto text-center">
                             <a href="https://www.youtube.com/watch?v=asqnjPukYBQ" target="_blank" rel="noopener noreferrer">
@@ -31,11 +32,14 @@ const Hero = () => {
                                 </span>
                             </a>
                         </div>
-                        <div className="w-11/12 max-w-sm mx-auto text-center rounded bg-white py-4 flex flex-col justify-center items-center gap-12 lg:max-w-xl">
+                        <div className="w-11/12 max-w-sm mx-auto text-center rounded bg-white py-4 flex flex-col justify-center items-center gap-4 lg:max-w-xl">
                             <h1 className="w-4/5 mx-auto text-2xl uppercase font-semibold text-[#233F4D] lg:text-4xl">Help Us Achive Our Goal</h1>
-                            <Link to='/donate' className="w-max mx-auto grid place-content-center">
-                                <img src={razorpayBtn} alt="Donate" className='w-44 md:w-60 lg:w-80' />
-                            </Link>
+                            <img src={qr} alt="Donate" className='w-44 md:w-60' />
+                            <p className="text-black text-lg font-semibold md:text-xl lg:text-2xl">OR</p>
+                            <span className="text-[#233F4D] font-medium md:text-lg lg:text-xl">
+                                UPI ID:
+                                <p className="border-b border-b-[#233F4D] font-semibold px-1">raunak.maini55@okaxis</p>
+                            </span>
                         </div>
                     </div>
                 </div>
